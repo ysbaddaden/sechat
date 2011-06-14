ba30056a2190086bba2971ffc9ac66cfcc5f567b
Julien Portalier <ysbaddaden@gmail.com>
2011-06-14 18:09:15 +0200

    Wrote tests for Question scopes.

--- test/question_test.rb
+++ test/question_test.rb
@@ -14,11 +14,17 @@ class QuestionTest < ActiveSupport::TestCase
   end
 
   test "latest scope" do
+    assert_equal [questions(:replied), questions(:multi_answered), questions(:unreplied), questions(:answered)],
+      Question.latest
   end
 
   test "answered scope" do
+    assert_equal [questions(:multi_answered), questions(:answered)],
+      Question.answered.latest
   end
 
   test "unanswered scope" do
+    assert_equal [questions(:replied), questions(:unreplied)],
+      Question.unanswered.latest
   end
 end
